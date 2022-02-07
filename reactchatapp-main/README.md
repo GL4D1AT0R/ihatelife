2dc9abdf398cfc7a503a98b6f234f361cefba85f
DEV0616 <84785529+DEV0616@users.noreply.github.com>
2022-02-06 21:48:55 -0500

    added project form local host to netlify so
    ...

---
+++ reactchatapp-main/README.md
@@ -0,0 +1,25 @@
+# Installation 
+npm install @ant-design/icons axios react-chat-engine
+
+npm start
+# Getting Started with Create React App
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+Backend is hosted by  https://chatengine.io
+For changing it to your personal chat-app you have to replace the api key to yours.
+©Coded by Eljan Guluyev 2021.
+# Live Demo
+
+https://eljanschatapp.netlify.app
+
+# Chat with me
+
+
+- Login: customer
+
+
+- Password: supersecret
+
+
---
+++ reactchatapp-main/src/App.js
@@ -0,0 +1,26 @@
+  
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = 'f296791d-2d6d-4165-b26d-d049149a8386';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+// Logout elemey ucun ctrl shift i sonra terminalda applicationa basin local serveri temizleyin
+
+export default App;
